dc2cf4d28a935ad6c92b1d63e7fdc6798b0cc5a1
Chris Dijkshoorn <c.r.dijkshoorn@vu.nl>
2016-07-26 09:46:37 +0200

    Added two stub files for handling platform information.

---
+++ src/helpers/platforms.js
@@ -0,0 +1,8 @@
+/*******************************************************************************
+DigiBird Platforms
+
+Functions used to retrieve information about the different platforms
+incoorporated in DigiBird
+*******************************************************************************/
+module.exports = {
+};
