d6a8ab067235e81414ca8857b2e9e5856542dabf
Chris Dijkshoorn <c.r.dijkshoorn@vu.nl>
2016-08-15 16:15:08 +0200

    Added footer to app.

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -8,7 +8,6 @@ import NavItem from 'react-bootstrap/lib/NavItem';
 import Helmet from 'react-helmet';
 import { isLoaded as isInfoLoaded, load as loadInfo } from 'redux/modules/info';
 import { isLoaded as isAuthLoaded, load as loadAuth, logout } from 'redux/modules/auth';
-import { InfoBar } from 'components';
 import { push } from 'react-router-redux';
 import config from '../../config';
 import { asyncConnect } from 'redux-async-connect';
@@ -88,13 +87,13 @@ export default class App extends Component {
         <div className={styles.appContent}>
           {this.props.children}
         </div>
-        <InfoBar/>
 
-        <div className="well text-center">
-          Have questions? Ask for help <a
-          href="https://github.com/erikras/react-redux-universal-hot-example/issues"
-          target="_blank">on Github</a> or in the <a
-          href="https://discord.gg/0ZcbPKXt5bZZb1Ko" target="_blank">#react-redux-universal</a> Discord channel.
+        <div className="container footer">
+            <div className="row">
+                <div className="col-md-8 col-md-offset-2">
+                    <p>DigiBird is a valorisation project funded by the Dutch national program <a href="http://www.commit-nl.nl"><img className="footerLogo" src="img/logos/commit.png"></img></a></p>
+                </div>
+            </div>
         </div>
       </div>
     );
