3005c46767ba5dbf93e7af53b9e8041f0f8de797
Chris Dijkshoorn <c.r.dijkshoorn@vu.nl>
2016-08-16 10:49:54 +0200

    Removed login pages.

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,6 +1,5 @@
 export App from './App/App';
 export Blog from './Blog/Blog';
 export Home from './Home/Home';
-export LoginSuccess from './LoginSuccess/LoginSuccess';
 export People from './People/People';
 export NotFound from './NotFound/NotFound';
--- src/routes.js
+++ src/routes.js
@@ -1,33 +1,14 @@
 import React from 'react';
 import {IndexRoute, Route} from 'react-router';
-import { isLoaded as isAuthLoaded, load as loadAuth } from 'redux/modules/auth';
 import {
     App,
     Blog,
     Home,
-    LoginSuccess,
     People,
     NotFound,
   } from 'containers';
 
-export default (store) => {
-  const requireLogin = (nextState, replace, cb) => {
-    function checkAuth() {
-      const { auth: { user }} = store.getState();
-      if (!user) {
-        // oops, not logged in, so can't be here!
-        replace('/');
-      }
-      cb();
-    }
-
-    if (!isAuthLoaded(store.getState())) {
-      store.dispatch(loadAuth()).then(checkAuth);
-    } else {
-      checkAuth();
-    }
-  };
-
+export default () => {
   /**
    * Please keep routes in alphabetical order
    */
@@ -35,11 +16,6 @@ export default (store) => {
     <Route path="/" component={App}>
       { /* Home (main) route */ }
       <IndexRoute component={Home}/>
-
-      { /* Routes requiring login */ }
-      <Route onEnter={requireLogin}>
-        <Route path="loginSuccess" component={LoginSuccess}/>
-      </Route>
 
       { /* Routes */ }
       <Route path="blog" component={Blog}/>
