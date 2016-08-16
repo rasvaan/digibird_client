7ea9a1b9fda9ae5e44b7ca90e0b373ea0064ef0f
Chris Dijkshoorn <c.r.dijkshoorn@vu.nl>
2016-08-16 20:36:30 +0200

    Made 404 page pretty.

--- src/containers/NotFound/NotFound.js
+++ src/containers/NotFound/NotFound.js
@@ -1,10 +1,8 @@
 import React from 'react';
+import { Banner } from 'components';
 
 export default function NotFound() {
   return (
-    <div className="container">
-      <h1>404 Error</h1>
-      <p>Page not found.</p>
-    </div>
+    <Banner title="404 Error: Page not found." image="error" />
   );
 }
