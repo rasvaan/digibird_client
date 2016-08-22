e169ea8107ac76b8cef6c9aae2746afe0ecded16
Chris Dijkshoorn <c.r.dijkshoorn@vu.nl>
2016-08-22 13:10:09 +0200

    Removed reducers.

--- src/redux/modules/reducer.js
+++ src/redux/modules/reducer.js
@@ -1,26 +1,8 @@
 import { combineReducers } from 'redux';
-import multireducer from 'multireducer';
 import { routerReducer } from 'react-router-redux';
 import {reducer as reduxAsyncConnect} from 'redux-async-connect';
 
-import auth from './auth';
-import counter from './counter';
-import {reducer as form} from 'redux-form';
-import info from './info';
-import widgets from './widgets';
-import blog from './blog';
-
 export default combineReducers({
   routing: routerReducer,
-  reduxAsyncConnect,
-  auth,
-  form,
-  multireducer: multireducer({
-    counter1: counter,
-    counter2: counter,
-    counter3: counter
-  }),
-  info,
-  widgets,
-  blog
+  reduxAsyncConnect
 });
