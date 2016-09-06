dc4563aaf47ce71516995f4770d4831c5e1ea5d1
Chris Dijkshoorn <c.r.dijkshoorn@vu.nl>
2016-09-06 11:58:45 +0200

    Removed statistics reference in reducer.

--- src/redux/modules/reducer.js
+++ src/redux/modules/reducer.js
@@ -3,7 +3,6 @@ import { routerReducer } from 'react-router-redux';
 import {reducer as reduxAsyncConnect} from 'redux-connect';
 import blog from './blog.js';
 import monitor from './monitor.js';
-import statistics from './statistics.js';
 
 export default combineReducers({
   routing: routerReducer,
