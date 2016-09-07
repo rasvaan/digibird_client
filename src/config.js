5e3634d29893dcb6bcd5937ec6c7aa0e938aa3db
Chris Dijkshoorn <c.r.dijkshoorn@vu.nl>
2016-09-07 09:40:45 +0200

    Cleanup.

--- src/config.js
+++ src/config.js
@@ -16,20 +16,20 @@ module.exports = Object.assign({
   apiPort: process.env.APIPORT,
   app: {
     title: 'DigiBird',
-    description: 'All the modern best practices in one example.',
+    description: 'DigiBird on the fly collection integration.',
     head: {
       titleTemplate: 'DigiBird %s',
       meta: [
-        {name: 'description', content: 'All the modern best practices in one example.'},
+        {name: 'description', content: 'DigiBird on the fly collection integration.'},
         {charset: 'utf-8'},
-        {property: 'og:site_name', content: 'React Redux Example'},
-        {property: 'og:image', content: 'https://react-redux.herokuapp.com/logo.jpg'},
+        {property: 'og:site_name', content: 'DigiBird'},
+        {property: 'og:image', content: 'http://www.digibird.org/favicon.png'},
         {property: 'og:locale', content: 'en_US'},
-        {property: 'og:title', content: 'React Redux Example'},
-        {property: 'og:description', content: 'All the modern best practices in one example.'},
+        {property: 'og:title', content: 'DigiBird'},
+        {property: 'og:description', content: 'DigiBird on the fly collection integration.'},
         {property: 'og:card', content: 'summary'},
-        {property: 'og:site', content: '@erikras'},
-        {property: 'og:creator', content: '@erikras'},
+        {property: 'og:site', content: '@rasvaan'},
+        {property: 'og:creator', content: '@rasvaan'},
         {property: 'og:image:width', content: '200'},
         {property: 'og:image:height', content: '200'}
       ]
