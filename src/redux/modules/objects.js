0d98a15eaaa420deae40daaacf2d8a8030945c0d
Chris Dijkshoorn <c.r.dijkshoorn@vu.nl>
2016-10-20 16:12:18 +0200

    Considering page parameters, resolves #108.

--- src/redux/modules/objects.js
+++ src/redux/modules/objects.js
@@ -40,10 +40,24 @@ export default function objects(state = initialState, action = {}) {
   }
 }
 
-export function loadObjects(platform) {
+export function loadObjects(platform, query) {
+  let url;
+  const api = `/api/objects?platform=${platform}`;
+
+  if (query.common_name) {
+    url = `${api}&common_name=${query.common_name}`;
+  } else if (query.genus && !query.species) {
+    url = `${api}&genus=${query.genus}`;
+  } else if (query.genus && query.species) {
+    url = `${api}&genus=${query.genus}&species=${query.species}`;
+  } else {
+    // who doesn't like Bubo bubo?
+    url = `${api}&genus=bubo&species=bubo`;
+  }
+
   return {
     types: [LOAD, LOAD_SUCCESS, LOAD_FAIL],
     platform: platform,
-    promise: (client) => client.get(`/api/objects?platform=${platform}&genus=pica&species=pica`)
+    promise: (client) => client.get(url)
   };
 }
