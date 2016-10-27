b3290f1af479e578d2030d2e0882c45ac17528d8
Chris Dijkshoorn <c.r.dijkshoorn@vu.nl>
2016-10-27 13:13:51 +0200

    Sorting results in the frontend.

--- src/containers/Annotations/Annotations.js
+++ src/containers/Annotations/Annotations.js
@@ -12,6 +12,10 @@ import Helmet from 'react-helmet';
     accuratorLoading: state.annotations.accurator.loading,
     accuratorLoaded: state.annotations.accurator.loaded,
     accuratorLoadingAt: state.annotations.accurator.loadingAt,
+    waisdaResults: state.annotations.waisda.results,
+    waisdaLoading: state.annotations.waisda.loading,
+    waisdaLoaded: state.annotations.waisda.loaded,
+    waisdaLoadingAt: state.annotations.waisda.loadingAt,
   }),
   {updateAnnotations}
 )
@@ -21,6 +25,11 @@ import Helmet from 'react-helmet';
       return dispatch(loadAnnotations('accurator'));
     },
   },
+  {
+    promise: ({store: {dispatch}}) => {
+      return dispatch(loadAnnotations('waisda'));
+    },
+  },
 ])
 export default class Annotations extends Component {
   static propTypes = {
@@ -28,11 +37,15 @@ export default class Annotations extends Component {
     accuratorLoading: PropTypes.bool,
     accuratorLoaded: PropTypes.bool,
     accuratorLoadingAt: PropTypes.string,
+    waisdaResults: PropTypes.array,
+    waisdaLoading: PropTypes.bool,
+    waisdaLoaded: PropTypes.bool,
+    waisdaLoadingAt: PropTypes.string,
     updateAnnotations: PropTypes.func
   }
   componentWillMount() {
     // set update interval
-    this.intervalId = setInterval(this.updateAccurator.bind(this), 5000);
+    this.intervalId = setInterval(this.updateAccurator.bind(this), 5000000);
   }
   componentWillUnmount() {
     // use intervalId from the state to clear the interval
@@ -43,8 +56,8 @@ export default class Annotations extends Component {
     const { accuratorLoadingAt } = this.props;
     this.props.updateAnnotations('accurator', accuratorLoadingAt);
   }
-  accuratorNodes(accuratorResults) {
-    return accuratorResults.map((result) => {
+  createNodes(results) {
+    return results.map((result) => {
       return (
         <Media
           key={result['edm:aggregatedCHO']['@id']}
@@ -56,18 +69,49 @@ export default class Annotations extends Component {
       );
     });
   }
+  sortAnnotations(aggregations) {
+    // sort annotation lists
+    aggregations.forEach(aggregation => {
+      if (aggregation['edm:aggregatedCHO'].annotations) {
+        aggregation['edm:aggregatedCHO'].annotations.sort((one, two) => {
+          if (one['oa:annotatedAt'] < two['oa:annotatedAt']) return 1;
+          if (one['oa:annotatedAt'] > two['oa:annotatedAt']) return -1;
+          return 0; // one must be equal to two
+        });
+      }
+    });
+    return aggregations;
+  }
+  sortResults(aggregations) {
+    console.log('FIRST RESUKT', aggregations[0]);
+    // sort aggregations based on first entry annotation list
+    return aggregations.sort((one, two) => {
+      const dateOne = one['edm:aggregatedCHO'].annotations[0]['oa:annotatedAt'];
+      const dateTwo = two['edm:aggregatedCHO'].annotations[0]['oa:annotatedAt'];
+
+      if (dateOne < dateTwo) return 1;
+      if (dateOne > dateTwo) return -1;
+      return 0; // dateOne must be equal to dateTwo
+    });
+  }
   render() {
     const styles = require('./Annotations.scss');
-    const { accuratorResults, accuratorLoaded } = this.props;
-    let accuratorNodes;
+    const { accuratorResults, accuratorLoaded, waisdaResults, waisdaLoaded } = this.props;
+    let results = [];
+    let nodes;
 
-    if (accuratorLoaded) accuratorNodes = this.accuratorNodes(accuratorResults);
+    if (accuratorLoaded) results = results.concat(accuratorResults);
+    if (waisdaLoaded) results = results.concat(waisdaResults);
+
+    results = this.sortAnnotations(results);
+    results = this.sortResults(results);
+    nodes = this.createNodes(results);
 
     return (
       <div>
         <Helmet title="Annotations"/>
         <div className={`container-fluid  ${styles.noGutter} ${styles.noPadding}`}>
-          {accuratorNodes}
+          {nodes}
         </div>
       </div>
     );
